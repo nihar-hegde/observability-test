e80a8b021a781d7d750ad893d911c268b848ffaf
Nihar Hegde <niharhegde163@gmail.com>
2026-05-24 20:45:19 +0530

    enums

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Quit => self.quit(),
+            Message::Move(point) => self.move_position(point),
+            Message::ChangeColor(red, green, blue) => self.change_color(red, green, blue),
+            Message::Echo(s) => self.echo(s),
+            Message::Resize { width, height } => self.resize(width, height),
+        }
     }
 }
 
